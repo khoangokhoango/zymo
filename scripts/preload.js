ec61929b45c54d07121b463e09542d4c3b9d6ec9
Khoa Ngo <ngokhoa9596@gmail.com>
2024-05-21 22:45:27 +0700

    update

---
+++ scripts/preload.js
@@ -0,0 +1,35 @@
+import data from "./data.json";
+
+customElements.define("zymo-cards", class ZymoCards extends HTMLElement {
+  constructor() {
+    super();
+    this.data = data ? Object.entries(data) : [];
+    this.handleGenerateCardsList();
+  }
+
+  handleGenerateCardTemplate(card) {
+    return `
+      <li class="zymo-cards__item">
+        <article class="zymo-card">
+          <div class="zymo-card__inner">
+            <div class="zymo-card__image">
+            <img src="${card.image_url}" alt="${card.title}" />
+            </div>
+            <div class="zymo-card__information">
+              <h2 class="zymo-heading--4 zymo-card__heading">${card.title}</h2>
+              <p class="zymo-body-text zymo-card__description">${card.description}</p>
+              <a href="${card.url}" class="zymo-button--secondary zymo-card__cta">Learn More</a>
+            </div>
+          </div>
+        </article>
+      </li>
+    `;
+  }
+
+  handleGenerateCardsList() {
+    const ELM_listItems = this.querySelector('ul');
+    this.data.forEach(([_, value]) => {
+      ELM_listItems.innerHTML += this.handleGenerateCardTemplate(value);
+    })
+  }
+})
