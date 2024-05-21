8c02092119608a803274a56cde24953e8f3e554b
Khoa Ngo <ngokhoa9596@gmail.com>
2024-05-22 01:18:24 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,21 @@
 // Handle form submission
 (function(){
   const contactForm = document.querySelector('#ContactForm form');
+  const messageField = document.querySelector('.zymo-contact-form__message');
   const requiredMessage = 'This field is required';
   contactForm.addEventListener('submit', (e) => {
-    [
-      "FirstName", "LastName", "Company", "Role"
-    ].forEach(field => {
-      e.preventDefault();
+    e.preventDefault();
+    let hasError = false;
+    [ "FirstName", "LastName", "Company", "Role"].forEach(field => {
       const input = contactForm.querySelector(`#ContactForm-${field}`);
       const note = input.nextElementSibling;
       input.closest('.zymo-form__field').classList.toggle('zymo-form__field--has-error', !input.value);
       note.textContent = !input.value ? requiredMessage : '';
+      hasError = !input.value;
     })
+
+    if (!hasError) {
+      messageField.classList.remove('d-none')
+    }
   })
 })();
